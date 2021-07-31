205a9a8166e77c90ee18c149e96e265271e755e6
Jacob Bacon <jacobacon95@yahoo.com>
2021-07-31 16:40:04 -0500

    Hooked into words API, saving synonym to state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import vader from 'vader-sentiment';
 import React, {useState} from 'react';
+import fetchSynonym from './utils/wordAPIService';
 import './App.css';
 
+require('dotenv').config();
 
 
 function App() {
   const [userInput, setUserInput] = useState('');
   const [sentimentScore, setSentimentScore] = useState('');
+  const [synonym, setSynonym] = useState([])
+  
 
   const findSentimentScore = () => {
     const intensity = vader.SentimentIntensityAnalyzer.polarity_scores(userInput);
@@ -18,16 +22,24 @@ function App() {
     setUserInput(event.target.value);
   }
 
+  const findSynonym = () => {
+    console.log('key', process.env.WORD_API_KEY);
+
+    fetchSynonym(userInput).then((value) => {
+      setSynonym(value.data);
+      console.log('Synonym', synonym);
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        
- 
         <input value={userInput} onChange={handleChange} placeholder="Type Here"></input>
         <button onClick={findSentimentScore}>Submit</button>
+        <button onClick={findSynonym}>Synonyze</button>
         <p>
           {sentimentScore}
-          {console.log(userInput)}
+          {/* {console.log(userInput)} */}
         </p>
       </header>
     </div>
